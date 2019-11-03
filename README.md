b2ce9d553d897b4f3919b8fe03eaa9593682c3b4
Student <calum_darroch@hotmail.com>
2019-11-03 16:09:55 +0000

    First test passed

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+The goal of this exercise is to test the functionality of the  posts path of the [JSON Placeholder API](https://github.com/typicode/jsonplaceholder).
--- test/test.js
+++ test/test.js
@@ -1,8 +1,16 @@
+const fetch = require('node-fetch');
 var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
+var expect = require('chai').expect;
+
+describe('/posts', function() {
+  it('returns posts as JSON', async () => {
+    await fetch('http://jsonplaceholder.typicode.com/posts')
+      .then((res) => {
+        return res.json()
+      })
+      .then((res) => {
+        // console.log(res);
+        expect(res).to.be.a('Array');
+      });
   });
 });
