97dbff018b87166a1dd65e007b0bf65911838eed
Student <calum_darroch@hotmail.com>
2019-11-04 10:09:39 +0000

    Testing posts/1 for HTTP and HTTPS protocol calls

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+# Testing JSON Placeholder API
 
+The goal of this exercise is to test the functionality of the `posts` path of the [JSON Placeholder API](https://github.com/typicode/jsonplaceholder), particularly:
 
-The goal of this exercise is to test the functionality of the  posts path of the [JSON Placeholder API](https://github.com/typicode/jsonplaceholder).
+GET /posts/1
+GET /posts/1/comments
+
+The criteria for the tests were that they must be automated. Further criteria - such as particular techniques or frameworks to use, parameters under test etc. - were not supplied. As such, I opted to build my tests using the following stack:
+
+- [Node.js](https://nodejs.org/en/)
+- [node-fetch](https://www.npmjs.com/package/node-fetch) npm package
+- [Mocha](https://mochajs.org/) testing framework
+- [Chai](https://www.chaijs.com/) assertion library
+- [SuperTest](https://github.com/visionmedia/supertest) library
+
+## Findings
+
+### posts/1 path
+- The posts will be returned via both the HTTP (port: 80) and HTTPS (port: 443) protocols. This is not secure.
---
+++ test/postTest.js
@@ -0,0 +1,44 @@
+const fetch = require('node-fetch');
+var assert = require('assert');
+var expect = require('chai').expect;
+
+describe('/posts/1', function() {
+  it('The API return the post as JSON', async () => {
+    await fetch('https://jsonplaceholder.typicode.com/posts/1', {
+      method: 'GET'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((response) => {
+        // console.log(response);
+        expect(response).to.be.an('Object');
+      });
+  });
+
+  it('Posts are returned using an insecure HTTP GET request', async () => {
+    await fetch('http://jsonplaceholder.typicode.com:80/posts/1', {
+      method: 'GET'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((response) => {
+        // console.log(response);
+        expect(response).to.be.an('Object');
+      });
+  });
+
+  it('Posts are returned using a secure HTTPS GET request', async () => {
+    await fetch('https://jsonplaceholder.typicode.com:443/posts/1', {
+      method: 'GET'
+    })
+      .then((response) => {
+        return response.json()
+      })
+      .then((response) => {
+        // console.log(response);
+        expect(response).to.be.an('Object');
+      });
+  });
+});
